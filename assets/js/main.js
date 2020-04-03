e36a852695e7c2ae14820164a2e56e7da5bbaf77
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-04-03 14:10:22 +0200

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+
+(function($) {
+
+	var	$window = $(window),
+		$body = $('body')
+
+	// Breakpoints.
+		breakpoints({
+			xlarge:	'(max-width: 1680px)',
+			large:	'(max-width: 1280px)',
+			medium:	'(max-width: 980px)',
+			small:	'(max-width: 736px)',
+			xsmall:	'(max-width: 480px)'
+		});
+
+	// Play initial animations on page load.
+		$window.on('load', function() {
+			window.setTimeout(function() {
+				$body.removeClass('is-preload');
+			}, 100);
+		});
+})(jQuery); 
