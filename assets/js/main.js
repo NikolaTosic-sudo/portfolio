7805b64abc7187ca4814929c2d2d933589144710
Nikola Tosic <55281244+NikolaTosic-sudo@users.noreply.github.com>
2020-08-10 16:43:11 +0200

    Initial commit
    Changing portfolio from scratch

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,22 @@
+/*
+	Photon by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+*/
 
 (function($) {
 
 	var	$window = $(window),
-		$body = $('body')
+		$body = $('body');
 
 	// Breakpoints.
 		breakpoints({
-			xlarge:	'(max-width: 1680px)',
-			large:	'(max-width: 1280px)',
-			medium:	'(max-width: 980px)',
-			small:	'(max-width: 736px)',
-			xsmall:	'(max-width: 480px)'
+			xlarge:   [ '1141px',  '1680px' ],
+			large:    [ '981px',   '1140px' ],
+			medium:   [ '737px',   '980px'  ],
+			small:    [ '481px',   '736px'  ],
+			xsmall:   [ '321px',   '480px'  ],
+			xxsmall:  [ null,      '320px'  ]
 		});
 
 	// Play initial animations on page load.
@@ -19,4 +25,8 @@
 				$body.removeClass('is-preload');
 			}, 100);
 		});
-})(jQuery); 
+
+	// Scrolly.
+		$('.scrolly').scrolly();
+
+})(jQuery);
